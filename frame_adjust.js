99797f0a54bdfbda14ebb2050f7c07fdbf2a9588
Mohsen Sadeghi <mohsen.sadeghi@fu-berlin.de>
2022-06-01 19:03:08 +0200

    small vis. modifications

--- frame_adjust.js
+++ frame_adjust.js
@@ -11,12 +11,12 @@ function FrameResize() {
 	F1.style.position = 'fixed';
 	F1.style.top = '10px';
 	F1.style.left = '10px';
-	F1.style.width = '200px';
+	F1.style.width = '250px';
 	F1.style.height = PageHeight - 20 + 'px';
 	
 	F2.style.position = 'absolute';
 	F2.style.top = '0px';
-	F2.style.left = '240px';
+	F2.style.left = '270px';
 	
 	F1.style.visibility = 'visible';
 	F2.style.visibility = 'visible';
