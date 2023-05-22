13ae374517d8e4b98ef11fcedc687f7581534419
Mohsen Sadeghi <mohsen.sadeghi@fu-berlin.de>
2023-05-22 18:27:35 +0200

    new flow

--- frame_adjust.js
+++ frame_adjust.js
@@ -1,4 +1,5 @@
 function FrameResize() {
+
 	var PageWidth;
 	var PageHeight;
 	
@@ -7,6 +8,7 @@ function FrameResize() {
 
 	var menu_div = parent.document.getElementById('MenuFrame');
 	var page_div = parent.document.getElementsByClassName('PageContent')[0];
+	var masthead_div = parent.document.getElementById('Masthead');
 	
 	menu_div.style.position = 'fixed';
 	menu_div.style.top = '10px';
@@ -19,6 +21,26 @@ function FrameResize() {
 	page_div.style.left = '270px';
 	page_div.style.right = '10px';
 	
+	if (masthead_div)
+	{
+	    var masthead_photo = parent.document.getElementById('Masthead_Photo');
+	    var masthead_name = parent.document.getElementById('Masthead_Name');
+        
+        var photo_aspect_ratio = masthead_photo.naturalWidth / masthead_photo.naturalHeight;
+        var name_aspect_ratio = masthead_name.naturalWidth / masthead_name.naturalHeight;
+        
+        var max_width = PageWidth - 295;
+        var best_width = Math.min(max_width, 1200);
+        
+    	masthead_photo.width = best_width;
+    	masthead_photo.height = best_width / photo_aspect_ratio;
+        
+        if (max_width < 80 * name_aspect_ratio)
+        {  	
+       	    masthead_name.width = max_width;
+       	    masthead_name.height = max_width / name_aspect_ratio;
+       	}
+	}
 	
 	if (page_div.offsetHeight < PageHeight - 10)
 	{
